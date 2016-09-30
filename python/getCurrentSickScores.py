8d88e043c699782c4228ff910717bf2b43c6ccae
Sunniel Yu <sunny22yu@gmail.com>
2016-09-30 14:01:51 -0400

    updated

--- python/getCurrentSickScores.py
+++ python/getCurrentSickScores.py
@@ -8,13 +8,13 @@ dataJSON = ""
 
 with open('zipcodes.csv') as f:
     csvRead = csv.reader(f, delimiter=',')
-        for row in csvRead:
-            zipMD = str(row[1])
-            params = "?api_key=GX3RD5Xx3wJmBSitk9Ee&country=US&zip="+zipMD+"&ids=1,2,4,7,15"
-            req = Request(sickScoreAPI+params)
-            response = urlopen(req)
-            response = response.read().decode('utf-8')
-            data += response + '\n'
+    for row in csvRead:
+        zipMD = str(row[1])
+        params = "?api_key=GX3RD5Xx3wJmBSitk9Ee&country=US&zip="+zipMD+"&ids=1,2,4,7,15"
+        req = Request(sickScoreAPI+params)
+        response = urlopen(req)
+        response = response.read().decode('utf-8')
+        data += response + '\n'
 
 
 arrData = data.split('\n')
@@ -22,7 +22,7 @@ arrData = data.split('\n')
 with open('currentSickScores.csv', 'w') as out:
     out.write('zip,flu,bronchitis,cold,strep,pneumonia\n')
     for u in arrData:
-        if(u! = "" and u[0] != '['):
+        if(u != "" and u[0] != '['):
             dataJSON = json.loads(u)
             a1,a2,a3,a4,a5 = "0", "0", "0", "0", "0"
             for a in dataJSON:
